60c196a3beee1553ef986c087a5369ad3fefbfbd
vmmadathil <madathilvisakh@gmail.com>
2021-01-19 15:55:06 -0600

    finished with form logic

--- src/app/pages/eligibility/eligibility.component.ts
+++ src/app/pages/eligibility/eligibility.component.ts
@@ -14,33 +14,89 @@ export class EligibilityComponent implements OnInit{
     //variables for questions
     hcWorker = false;
     ltresident = false;
+    sixFive = false;
+    chronic = false;
 
-
-
-    aEligibile = false;
-    bEligibile = false;
+    //variables for the divs
+    residentDiv = true;
+    sixfiveDiv = true;
+    chronicDiv = true;
+    aEligibileDiv = true;
+    bEligibileDiv = true;
+    ineligibileDiv = true;
 
     toggleHCWorkerYes() {
         this.hcWorker = true;
+        this.showASuccess();
+        this.showFail()
     }
 
     toggleHCWorkerNo() {
         this.hcWorker = false;
+        this.showASuccess();
+        this.residentDiv = false;
     }
 
     toggleLTRYes() {
         this.ltresident = true;
+        this.showASuccess();
+        this.showFail()
     }
 
     toggleLTRNo() {
         this.ltresident = false;
+        this.showASuccess();
+        this.sixfiveDiv = false;
+    }
+
+    toggleSixFiveYes() {
+        this.sixFive = true;
+        this.showBSuccess();
+        this.showFail()
+    }
+
+    toggleSixFiveNo() {
+        this.sixFive = false;
+        this.showBSuccess();
+        this.chronicDiv = false;
+    }
+
+    toggleChronicYes() {
+        this.chronic = true;
+        this.showBSuccess();
+        this.showFail()
+    }
+
+    toggleChronicNo() {
+        this.chronic = false;
+        this.showBSuccess();
+        this.showFail();
     }
 
     showASuccess() {
         if(this.hcWorker == true || this.ltresident == true) {
-            this.aEligibile = true;
+            this.aEligibileDiv = false;
+        }
+        else{
+            this.aEligibileDiv = true;
         }
     }
 
-    showBSuccess() {}
+    showBSuccess() {
+        if(this.sixFive == true || this.chronic == true){
+            this.bEligibileDiv = false;
+        }
+        else{
+            this.bEligibileDiv = true;
+        }
+    }
+
+    showFail(){
+        if(this.hcWorker == false && this.ltresident == false && this.sixFive == false && this.chronic == false){
+            this.ineligibileDiv = false;
+        }
+        else{
+            this.ineligibileDiv = true;
+        }
+    }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -12,9 +12,9 @@ export const ROUTES: RouteInfo[] = [
     { path: '/overview',      title: 'Overview',         icon:'nc-chart-bar-32',       class: '' },
     { path: '/eligibility',   title: 'Check Eligibility', icon:'nc-single-02',  class: '' },
     { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
-    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
+   // { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
+   // { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
+   // { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
 ];
 
 @Component({
