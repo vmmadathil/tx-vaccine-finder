476fb26af2474979bc4792d0d42aecfc9f9a498c
vmmadathil <madathilvisakh@gmail.com>
2021-01-19 14:25:26 -0600

    added html phase 1A eligibility

--- src/app/pages/eligibility/eligibility.component.ts
+++ src/app/pages/eligibility/eligibility.component.ts
@@ -9,4 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class EligibilityComponent implements OnInit{
     ngOnInit(){
     }
+
+
+    //variables for questions
+    hcWorker = false;
+    ltresident = false;
+
+
+
+    aEligibile = false;
+    bEligibile = false;
+
+    toggleHCWorkerYes() {
+        this.hcWorker = true;
+    }
+
+    toggleHCWorkerNo() {
+        this.hcWorker = false;
+    }
+
+    toggleLTRYes() {
+        this.ltresident = true;
+    }
+
+    toggleLTRNo() {
+        this.ltresident = false;
+    }
+
+    showASuccess() {
+        if(this.hcWorker == true || this.ltresident == true) {
+            this.aEligibile = true;
+        }
+    }
+
+    showBSuccess() {}
 }
